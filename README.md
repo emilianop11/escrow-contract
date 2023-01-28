0a30e6398c48b3a6e3693c41c5db6fba77d4fdaf
Emiliano Pelliccioni <emilianop11@gmail.com>
2023-01-28 15:22:27 -0300

    update deployment script

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
 npx hardhat run scripts/deploy.js --network matic
+
+// deploy locally
+npx hardhat run --network localhost scripts/deploy.js
 ```
-
-
-helper deployed contract 0xAC3d103b1Cc60a6D5453F8317D160A8DfE6302ba
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,18 @@
 //USDC 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174
 
 const main = async () => {
-  const helperContractFactory = await hre.ethers.getContractFactory('Helper');
-  const helperContract = await helperContractFactory.deploy("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174");
-  await helperContract.deployed();
-  console.log('Contract Address:', helperContract.address);
+  AnyToken = await ethers.getContractFactory('Any');
+  anyToken = await AnyToken.deploy();
+  await anyToken.deployed();
+  console.log('Token Address:', anyToken.address);
+
+  Escrow = await ethers.getContractFactory('Escrow');
+  escrow = await Escrow.deploy(anyToken.address);
+  await escrow.deployed();
+  console.log('Escrow Address:', escrow.address);
+
+  
+
 };
 
 const runMain = async () => {
